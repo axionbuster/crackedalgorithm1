4277d8864a4a955ced2756cf2d3856b3fc5bf222
axionbuster <axionbuster@gmail.com>
2024-12-20 16:40:47 -0800

    WIP work out NaN resilience

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,36 +2,50 @@ module Lib (march) where
 
 import Control.Monad.Fix
 import Control.Monad.ST.Lazy
-import Control.Monad.Zip
 import Data.Functor.Rep
 import Data.STRef.Lazy
 import Prelude hiding (read)
 
--- | March along a line segment, finding all intersections
+-- | march along a line segment, finding all intersections
 -- with grid points
+--
+-- the first point is always the starting point
+--
+-- the returned list being infinite, it is recommended to
+-- use 'take' to limit the number of points to be computed
 march ::
   forall f a.
-  ( RealFrac a,
+  ( Applicative f,
     Foldable f,
     Representable f,
-    MonadZip f
+    RealFloat a
   ) =>
-  f a -> f a -> [f a]
+  -- | starting point
+  f a ->
+  -- | direction (no need to be normalized)
+  f a ->
+  -- | list of points
+  [f a]
 march start direction = runST do
   let fi = fromIntegral :: Int -> a
       new = newSTRef
       read = readSTRef
       modify = modifySTRef
+      minnonan a b
+        | isNaN a = b
+        | isNaN b = a
+        | otherwise = min a b -- if both are NaN, then pick any
   cur <- new start
   sig <- new $ signum <$> direction
   fix \this -> do
     let t cur' sig' i =
-          let nex' = mzipWith (\c s -> (fi . floor) c + s) cur' sig'
+          let nex' = tabulate @f \j ->
+                fi (floor $ cur' `index` j) + sig' `index` j
            in (index nex' i - index cur' i) / index direction i
     tim <- do
       cur' <- read cur
       sig' <- read sig
-      pure $ minimum $ tabulate @f (t cur' sig')
-    modify cur (liftA2 (+) $ (tim *) `fmap` direction)
+      pure $ foldr1 minnonan $ tabulate @f (t cur' sig')
     cur' <- read cur
+    modify cur (liftA2 (+) $ (tim *) `fmap` direction)
     (cur' :) <$> this
