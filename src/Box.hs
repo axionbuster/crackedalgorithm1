8d49bba03cd9cbe46ee53d8cc1571b1f6d89ab8e
axionbuster <axionbuster@gmail.com>
2024-12-23 12:18:03 -0800

    WIP Collision detection
    will add resolution soon
    
    algorithm of hitting due to Bravery (X @onehundred28, GitHub @2tothe4th)

---
+++ src/Box.hs
@@ -0,0 +1,70 @@
+-- | a simple 3D box
+module Box
+  ( Box (..),
+    _dimensions,
+    _center,
+    boxzero,
+    locorner,
+    hicorner,
+  )
+where
+
+import Control.Lens
+import Control.Monad.Zip
+import Linear
+import Shape
+
+-- | a box in 3D space, located either relatively or absolutely
+data Box a = Box
+  { -- | the dimensions of the box
+    dimensions :: !(V3 a),
+    -- | the center of the box
+    center :: !(V3 a)
+  }
+  deriving stock (Show)
+
+-- | Lens for the dimensions of the box
+_dimensions :: Lens' (Box a) (V3 a)
+_dimensions = lens dimensions \b d -> b {dimensions = d}
+
+-- | Lens for the center of the box
+_center :: Lens' (Box a) (V3 a)
+_center = lens center \b c -> b {center = c}
+
+instance Shape Box where
+  hitting moving this that =
+    let (V3 x0_ y0_ z0_, V3 x1_ y1_ z1_) = (center this, center this + moving)
+        (x0, x1, y0, y1, z0, z1) =
+          ( min x0_ x1_,
+            max x0_ x1_,
+            min y0_ y1_,
+            max y0_ y1_,
+            min z0_ z1_,
+            max z0_ z1_
+          )
+        lc = locorner that
+        hc = hicorner that
+        d = dimensions this
+        (V3 u0 v0 w0, V3 u1 v1 w1) = (lc - d ^/ 2, hc + d ^/ 2)
+        (dx, dy, dz) = (x1 - x0, y1 - y0, z1 - z0)
+        (V2 tx0 tx1, V2 ty0 ty1, V2 tz0 tz1) =
+          ( (V2 u0 u1 - pure x0) ^/ dx,
+            (V2 v0 v1 - pure y0) ^/ dy,
+            (V2 w0 w1 - pure z0) ^/ dz
+          )
+     in maximum [tx0, ty0, tz0, 0] < minimum [tx1, ty1, tz1, 1]
+  intersecting this that =
+    let lotest = and $ mzipWith (<) (locorner this) (hicorner that)
+        hitest = and $ mzipWith (>) (hicorner this) (locorner that)
+     in lotest && hitest
+  relative box = V2 (locorner box) (hicorner box)
+  translate displacement box = box {center = displacement + center box}
+
+boxzero :: (Num a) => Box a
+boxzero = Box zero zero
+
+locorner :: (Fractional a) => Box a -> V3 a
+locorner (Box d c) = c - d ^/ 2
+
+hicorner :: (Fractional a) => Box a -> V3 a
+hicorner (Box d c) = c + d ^/ 2
---
+++ src/Shape.hs
@@ -0,0 +1,54 @@
+{-# LANGUAGE MonoLocalBinds #-}
+
+-- | a module for defining the @Shape@ typeclass
+module Shape (Shape (..), SomeShape1 (..), castshape1, Hit (..)) where
+
+import Data.Data
+import Linear (V2, V3)
+
+-- | a collision resolution data type
+data Hit a = Hit
+  { -- | proportion of move completed in [0, 1]
+    hitprop :: a,
+    -- | normal vector of the surface hit
+    --
+    -- a signum vector, so each component is either -1, 0, or 1
+    hitnorm :: V3 a,
+    -- | exact meaning and purpose unknown
+    hitcollidedshapexyz :: V3 a,
+    -- | what shape was hit (?)
+    hitcollidedshape :: SomeShape1 a
+  }
+  deriving (Show)
+
+-- | existential 'Shape' type but numeric type is erased
+data SomeShape1 a
+  = forall s.
+    ( Typeable (s a),
+      Show (s a),
+      Shape s
+    ) =>
+    SomeShape1 (s a)
+
+instance Show (SomeShape1 a) where
+  show (SomeShape1 s) = show s
+
+-- | cast a 'SomeShape1' to a specific type
+castshape1 :: (Typeable b) => SomeShape1 a -> Maybe b
+castshape1 (SomeShape1 s) = cast s
+
+-- | a translation of the @Shape@ interface from the original code
+class Shape s where
+  -- | check if two shapes intersect
+  intersecting :: (Fractional a, Ord a) => s a -> s a -> Bool
+
+  -- | check if the first shape will collide into the second shape
+  -- if it moves with the given displacement
+  hitting :: (Fractional a, Ord a) => V3 a -> s a -> s a -> Bool
+
+  -- | return the relative starting and ending positions of the shape
+  -- respectively
+  relative :: (Fractional a) => s a -> V2 (V3 a)
+
+  -- | translate the shape by the given displacement
+  translate :: (Num a) => V3 a -> s a -> s a
